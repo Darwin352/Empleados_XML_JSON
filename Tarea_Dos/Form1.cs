ed27779d4819705aaf0b1412211c17a26c92ce51
Darwin <Darwin@DESKTOP-L2IKD4N>
2024-03-12 13:38:04 -0500

    Agregar archivos de proyecto.

---
+++ Tarea_Dos/Form1.cs
@@ -0,0 +1,45 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace Tarea_Dos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dataSet1.Clear();
+            string filePath = "datos.xml";
+            dataSet1.ReadXml(filePath);
+            DgEmpleados.DataSource = dataSet1;
+            DgEmpleados.DataMember = "Empleado";
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string fileJson = File.ReadAllText("datos.json");
+            DataTable dt = (DataTable)JsonConvert.DeserializeObject(fileJson, typeof(DataTable));
+            DgEmpleados.DataSource = dt;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Dispose();
+        }
+    }
+}
